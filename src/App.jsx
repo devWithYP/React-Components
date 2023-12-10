b4c1c1ddc4428177ac6e413b10f1790c0100643b
devWithYP <ypprogramer007@gmail.com>
2023-12-10 20:24:13 +0530

    Components uploaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center py-4 mt-[40vh] bg-yellow-200">
+    <h1 className="items-center text-3xl font-bold text-gray-900">
+      This are React and Tailwind CSS components, feel free to use them in your Project.
+    </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
+  },
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [ require('@tailwindcss/forms'),
+             require('@tailwindcss/aspect-ratio'),
+                            
+   ],
+}
